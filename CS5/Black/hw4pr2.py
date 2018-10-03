ab69657e23c4fc0f5c7f1e8bed0d49b726dcca2e
Kevin Herrera <kherrera@hmc.edu>
2018-10-02 19:37:12 -0700

    cs5black lab and assignment

--- CS5/Black/hw4pr2.py
+++ CS5/Black/hw4pr2.py
@@ -2,7 +2,7 @@
 # Filename: hw4pr2.py
 # Name: Kevin Herrera 9/30/18
 # Problem description:
-
+from functools import reduce
 def numToBase(N, B):
     if N == 0:
         return ''
@@ -25,11 +25,35 @@ def add(S, T):
     return numToBase(snum+tnum, 2)
 
 # Helper Functions
-def convertToNum(C):
-    return numToBase(ord(C), 2)
+# def convertToNum(C):
+#     return numToBase(ord(C), 2)
 
-def createNumList(S):
-    return list(map(lambda x: convertToNum(x), S))
+# def createNumList(string):
+#     return list(map(lambda x: ord(x), string))
+
+def createBList(string):
+    return list(map(lambda x: numToBase(ord(x), 2), string))
+
+def create8BList(bitList):
+    return list(map(lambda x: '0'*(8-len(x))+x, bitList))
+
+def createNumList(bitList):
+    return list(map(lambda x: baseBToNum(x, 2), bitList))
+
+def xOr(bit1, bit2):
+    if bit1 == bit2:
+        return "0"
+    else:
+        return "1"
 
 def xorStrings(S1, S2):
-    return
+    bitList1 = createBList(S1)
+    bitList2 = createBList(S2)
+    
+    eightBitL1 = create8BList(bitList1)
+    eightBitL2 = create8BList(bitList2)
+
+    xOrList = list(map(lambda x, y: reduce(lambda m, n: m+n, list(map(lambda s, t: xOr(s, t), x, y))) , eightBitL1, eightBitL2))
+    numList = createNumList(xOrList)
+    
+    return reduce(lambda x, y: x+y, list(map(lambda x: chr(x), numList)))
---
+++ CS5/Black/hw4pr3.py
@@ -0,0 +1,118 @@
+# Your name here
+# Date
+# hw4pr3.py
+
+import math
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from functools import *
+
+# A sample 8x8 image represented as a sequence of 64 bits
+test1 = "0000001100001100000000110000001111111111111111111111111111111111"
+
+def powerOfTwo(num):
+    """ PROVIDED CODE. """
+    """ Takes a positive integer as input and returns True if and only if it is a power of two. """
+    if num == 1: return True
+    elif num % 2 == 1: return False
+    else: return powerOfTwo(num//2)
+
+def pngToArray(filename, threshold=2):
+    """ PROVIDED CODE. """
+    """ Takes a filename as input, where the file is a .png image, and returns a binary
+    2D array as output.  The image must be a square whose dimensions are a power of two. """
+    img=mpimg.imread(filename)  # read in the image
+    dimensions = img.shape  # Get the dimensions
+    rows = dimensions[0]    # Extract the number of rows...
+    columns = dimensions[1] # ... and columns
+    if rows != columns or not powerOfTwo(rows):  # check if the image is of appropriate dimensions
+        return None  
+    array = []  # start building the output array
+    for r in range(rows):
+        row = []
+        for c in range(columns):
+            if sum(img[r][c]) >= threshold:
+                row.append(0)
+            else:
+                row.append(1)
+        array.append(row)
+    return array
+
+def renderASCII(array):
+    """ PROVIDED CODE. """
+    """ Takes a 2D array of 0's and 1's as input and renders it as 0's and 1's on the screen  """
+    for row in array:
+        stringify = reduce(lambda X, Y: str(X) + str(Y), row)
+        print(stringify)
+            
+def renderImage(array):
+    """ PROVIDED CODE. """
+    """ Takes a 2D array of 0's and 1's as input and renders it on the screen using matplotlib. """
+    dim = len(array)
+    image = np.zeros((dim, dim), dtype = np.float)
+    for r in range(dim):
+        for c in range(dim):
+            image[r][c] = float(array[r][c]) 
+    plt.imshow(image, cmap="Greys", interpolation='nearest')
+    plt.show()
+    
+def stringToArray(bstring):
+    """ PROVIDED CODE. """
+    """ Takes a binary string as input and returns the 2D array representation of the image. """
+    dim = int(math.sqrt(len(bstring)))
+    charArray = [list(bstring[i:i+dim]) for i in range(0, len(bstring), dim)]
+    array = [ [int(x) for x in row] for row in charArray ]
+    return array
+
+def quadrants(array):
+    """ Takes an array of bits as input and returns a list of quadrants
+    of the form [NW, NE, SW, SE] where each entry is the array 
+    for that quadrant """
+    # You'll write this code!
+    return [NW, NE, SW, SE]
+
+def solidzero(array):
+    """ Takes a 2D binary array as input and returns True if every bit is a 0 and False otherwise. """
+    # You'll write this code.  One line suffices!
+
+def solidone(array):
+    """ Takes a 2D binary array as input and returns True if every bit is a 1 and False otherwise. """
+    # You'll write this code.  One line suffices!
+
+def makeQuadtree(array):
+    """ Returns a quadtree representation of the array. """
+    # You'll write this code
+    return quadtree
+
+def solidArray(value, pixels):
+    """ PROVIDED CODE. """
+    """ Takes a value (0 or 1) and a number of pixels and retursn a 2D array of picelsxpixels
+    bits all of which are set to the given value. """
+    return [[value]*pixels for row in range(pixels)]
+
+def makeArray(quadtree, dim):
+    """ Takes a quadree and dimension as input and
+    returns the 2D array representation of the quadtree """
+    # You'll write this code
+    return array
+
+def rotateRight(quadtree):
+    """ Takes a quadtuple as input and returns the quadtree that results when rotating that image
+    clockwise 90 degrees. """
+    # You'll write this code.  Around four lines of code suffice
+
+def flipHorizontal(quadtree):
+    """ Takes a quadtree as input and returns the quadtree that results when flipping the image
+    about the horizontal axis of symmetry. """
+    # You'll write this code.  Around four lines of code suffice
+
+def flipDiagonal(quadtree):
+    """ Takes a quadtree as input and returns the quadtree that results when flipping the image
+    about the diagonal line through the NE and SW corners of the image. """
+    # You'll write this code.  Around four lines of code suffice
+
+def invert(quadtree):
+    """ Takes a quadtree as input and returns the quadtree that results when flipping every white
+    pixel to a black pixel and vice versa. """
+    # You'll write this code.  Around four lines of code suffice
