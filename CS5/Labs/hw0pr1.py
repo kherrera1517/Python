44460888589297f210a2f75eb1319107b8a27568
Kevin Herrera <kherrera@hmc.edu>
2018-12-19 16:19:14 -0800

    picobot files

--- CS5/Labs/hw0pr1.py
+++ CS5/Labs/hw0pr1.py
@@ -5,15 +5,15 @@
 
 from math import *
 
-print("Zero is", 4+4-4-4)                           #prints 0
+print("Zero is", 4+4-4-4.0)                         #prints 0
 print("One is", 4%4+4/4)                            #prints 1
 print("Two is", 4/4+4/4)                            #prints 2
 print("Three is", (sqrt(4) + sqrt(4))/4 + sqrt(4))  #prints 3
-print("Four is", (4%4)%4+4)                         #prints 4
+print("Four is", (4%4)%4+4.0)                         #prints 4
 print("Five is", sqrt(4) + sqrt(4) + 4/4)           #prints 5
 print("Six is", 4%4+4+sqrt(4))                      #prints 6
 print("Seven is", 4+4/4+sqrt(4))                    #prints 7
-print("Eight is", 4%4+4+4)                          #prints 8
+print("Eight is", 4%4+4+4.0)                        #prints 8
 print("Nine is", 4/4+4+4)                           #prints 9
 print("Ten is", 4*4-4-sqrt(4))                      #prints 10
 print("Eleven is", (44)/(sqrt(4) + sqrt(4)))        #prints 11
@@ -21,7 +21,7 @@ print("Twelve is", (sqrt(4) + 4/4)*4)               #prints 12
 print("Thirteen is", 44/4 + sqrt(4))                #prints 13
 print("Fourteen is", (4*4) - (4/sqrt(4)))           #prints 14
 print("Fifteen is", 4*4 - 4/4)                      #prints 15
-print("Sixteen is", 4%4+4*4)                        #prints 16
+print("Sixteen is", 4%4+4*4.0)                        #prints 16
 print("Seventeen is", 4*4 + 4/4)                    #prints 17
 print("Eighteen is", 44/sqrt(4)-4)                  #prints 18
 print("Nineteen is", factorial(4)-(4+4/4))          #prints 19
